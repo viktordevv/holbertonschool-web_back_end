56f0046c76725699c851b31b8e36d914c799479b
Viktor <viktorkrilldj@gmail.com>
2022-11-28 18:38:34 -0500

    proba proba

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -4,9 +4,9 @@ export default function taskBlock(trueOrFalse) {
     const task2 = true;
 
     if (trueOrFalse) {
-      const task = true;
-      const task2 = false;
+        const task = true;
+        const task2 = false;
     }
 
     return [task, task2];
-  }
+}
