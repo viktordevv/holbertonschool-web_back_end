3be111881b48d49f5f068ce0ac6d3f6b40cb2c9a
Viktor <viktorkrilldj@gmail.com>
2022-11-28 18:02:55 -0500

    scope

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,3 +1,4 @@
+/* The best code */
 export function taskFirst() {
   const task = "I prefer const when I can.";
   return task;
---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+/* The best code */
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+  }
+
+  return [task, task2];
+}
