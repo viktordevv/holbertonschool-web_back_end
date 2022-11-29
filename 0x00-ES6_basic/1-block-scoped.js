abab673770881277a823266e420ed70dbe3cee77
Viktor <viktorkrilldj@gmail.com>
2022-11-29 14:45:45 -0500

    delete mains

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,11 @@
-/* The best code */
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
 
-    if (trueOrFalse) {
-        const task = true;
-        const task2 = false;
-    }
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
+}
 
-    return [task, task2];
+  return [task, task2];
 }
