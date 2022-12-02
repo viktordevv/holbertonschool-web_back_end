898c864046198387cefc7dc3dcd759ea97baa3d9
Viktor <viktorkrilldj@gmail.com>
2022-12-01 20:33:09 -0500

    fix bux

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   const workers = [];
-  for (const [department, employees] of Object.entries(report.allEmployees)) {
+  for (const [employees] of Object.entries(report.allEmployees)) {
     for (const employe of employees) {
       workers.push(employe);
     }
