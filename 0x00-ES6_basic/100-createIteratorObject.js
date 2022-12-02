805aa8d0cc195fa99ea9e4c446809433691b697f
Viktor <viktorkrilldj@gmail.com>
2022-12-01 19:52:08 -0500

    advances files

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,10 @@
+export default function createIteratorObject(report) {
+  const workers = [];
+  for (const [department, employees] of Object.entries(report.allEmployees)) {
+    for (const employe of employees) {
+      workers.push(employe);
+    }
+  }
+
+  return workers;
+}
